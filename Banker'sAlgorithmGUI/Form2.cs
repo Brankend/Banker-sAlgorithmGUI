6c8a7451330d138ff49c33daf6481bc035c65853
Brankend <brankend@yahoo.com>
2023-05-12 16:55:04 +0300

    Removed unused libraries

--- Banker'sAlgorithmGUI/Form2.cs
+++ Banker'sAlgorithmGUI/Form2.cs
@@ -1,15 +1,4 @@
-﻿using System;
-using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace Banker_sAlgorithmGUI
+﻿namespace Banker_sAlgorithmGUI
 {
 
 
